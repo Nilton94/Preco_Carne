4729e38a17bd7fcd849e5f7324277122e0c62d64
=Nilton94 <jose.nilton@ifood.com.br>
2025-03-30 11:42:00 -0300

    Testando Path

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,5 +1,6 @@
 # LIBS
 import pandas as pd
+import os
 import streamlit as st
 from utils.extracao_dados import extracao_dados
 from utils.utils_streamlit import get_metrics, get_widgets, get_table, get_calc, get_config
@@ -18,3 +19,5 @@ get_table()
 
 # CALCULADORA
 get_calc()
+
+st.write(os.getcwd())
